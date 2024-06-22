11dc3c0e622dd0ab5140f32362b653977b58adf6
Pol Brunet <pbrunetf@gmail.com>
2024-06-23 01:30:28 +0200

    Created Scripatble Object Card

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Card/New Card")]
+public class Card : ScriptableObject
+{
+    public enum Type {
+        [Description("Evento")]
+        Event,
+        [Description("Verdad")]
+        Truth,
+        [Description("Reto")]
+        Dare,
+        [Description("Qué prefieres?")]
+        Prefer,
+        [Description("BASO")]
+        BASO
+    }
+    public enum PriceType
+    {
+        [Description("Bebe")]
+        Sip,
+        [Description("Termina")]
+        Finish,
+        [Description("")]
+        None
+    }
+
+    [System.Serializable]
+    public struct Price
+    {
+        public PriceType type;
+        [Min(0)]
+        public int number;
+    }
+
+    public Type type;
+    public string content;
+    public Price price;
+}
---
+++ Assets/Scripts/CardEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using static Card;
+
+[CustomEditor(typeof(Card))]
+[CanEditMultipleObjects]
+public class CardEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var card = target as Card;
+
+        card.type = (Type)EditorGUILayout.EnumPopup("Type", card.type);
+        card.content = EditorGUILayout.TextField("Conent", card.content);
+        card.price.type = (PriceType)EditorGUILayout.EnumPopup("Price Type", card.price.type);
+        if (card.price.type != PriceType.None)
+            card.price.number = EditorGUILayout.IntField("Price", card.price.number);
+        if (card.price.number < 0) 
+            card.price.number = 0;
+    }
+}
