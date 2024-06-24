d55bb1ffea4946c4c6c0c1d94d48b3229153fe56
Pol Brunet <pbrunetf@gmail.com>
2024-06-24 17:48:22 +0200

    Actualizado editor para que se actualicen las constantes de los scriptableobjects en git

--- Assets/Scripts/CardEditor.cs
+++ Assets/Scripts/CardEditor.cs
@@ -1,22 +1,54 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
 using UnityEngine.Networking.Types;
 using static Card;
+using static UnityEngine.GraphicsBuffer;
 
 [CustomEditor(typeof(Card))]
 [CanEditMultipleObjects]
-public class CardEditor : Editor
-{
+public class CardEditor : Editor {
+    Card script;
+    List<SerializedProperty> sp;
+    SerializedProperty price;
+
+    void OnEnable()
+    {
+        script = (Card)target;
+        sp = new List<SerializedProperty>();
+        MemberInfo[] mi = script.GetType().GetFields();
+        for (int i = 0; i < mi.Length; i++)
+        {
+            sp.Add(serializedObject.FindProperty(mi[i].Name));
+            if (sp[i].type == "Price")
+            {
+                price = sp[i];
+            }
+        }
+    }
     public override void OnInspectorGUI()
     {
+        serializedObject.Update();
         var card = target as Card;
-
-        card.type = (CardType)EditorGUILayout.ObjectField(card.type, typeof(CardType), true);
-        if (card.type != null && card.type.hasPrice) {
-            card.price.type = (PriceType)EditorGUILayout.EnumPopup("Price Type", card.price.type);
-            card.price.number = EditorGUILayout.IntField("Price", card.price.number);
+        for (int i = 0; i < sp.Count; i++)
+        {
+            if (sp[i] == price)
+            {
+                if (script.type && script.type.hasPrice)
+                {
+                    EditorGUILayout.PropertyField(sp[i]);
+                } else
+                {
+                    price.FindPropertyRelative("number").intValue = 0;
+                    price.FindPropertyRelative("type").enumValueIndex = 0;
+                }
+            } else
+            {
+                EditorGUILayout.PropertyField(sp[i]);
+            }
         }
-        
-        if (card.price.number < 0) 
-            card.price.number = 0;
+
+        serializedObject.ApplyModifiedProperties();
     }
 }
